4d79e10c74db5713858526472e2b115b534524fe
Luis Filipe Soares Lima <141943390+lfslDEV@users.noreply.github.com>
2025-10-07 10:10:11 -0300

    Corrigido pagina de login

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from . import views
 from .views import busca_campos, solicitar_reserva, minhas_reservas
 
 
 urlpatterns = [
-    path('', views.busca_campos, name='login'),
-    path('busca/', busca_campos, name='busca_campos'),
+    path('', busca_campos, name='busca_campos'),
     path('reserva/<int:campo_id>/', solicitar_reserva, name='solicitar_reserva'),
     path('minhas_reservas/', minhas_reservas, name='minhas_reservas'),
 ]
--- locacao_campos/urls.py
+++ locacao_campos/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('contas/', include('django.contrib.auth.urls')),  # url para a autenticação de usuários
-    path('', include('core.urls')), # url para o app core
+    
+    path('contas/', include('django.contrib.auth.urls')),
+    
+    path('', include('core.urls')),
 ]
