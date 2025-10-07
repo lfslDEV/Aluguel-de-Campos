ab0611c9c6e8590aa758f4f90a6ac0aa7c2ffcd8
Luis Filipe Soares Lima <141943390+lfslDEV@users.noreply.github.com>
2025-10-07 10:43:36 -0300

    Adiciona funcionalidade de cancelar reserva, permitindo que usuários logados cancelem suas reservas

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import busca_campos, solicitar_reserva, minhas_reservas
+from .views import busca_campos, solicitar_reserva, minhas_reservas, cancelar_reserva
 
 
 urlpatterns = [
     path('', busca_campos, name='busca_campos'),
     path('reserva/<int:campo_id>/', solicitar_reserva, name='solicitar_reserva'),
     path('minhas_reservas/', minhas_reservas, name='minhas_reservas'),
+    path('cancelar_reserva/<int:reserva_id>/', cancelar_reserva, name='cancelar_reserva'),
 ]
