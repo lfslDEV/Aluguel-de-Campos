5e3c012384bafe734a520619eff22c2adb7ca708
Luis Filipe Soares Lima <141943390+lfslDEV@users.noreply.github.com>
2025-10-07 11:43:56 -0300

    Layout das paginas melhorado

--- locacao_campos/settings.py
+++ locacao_campos/settings.py
@@ -132,3 +132,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'busca_campos'
 LOGOUT_REDIRECT_URL = 'busca_campos'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
